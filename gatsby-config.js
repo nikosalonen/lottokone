cf4b3033e5d7c73b6d91f594bcc7aaa7259cb0cc
Niko Salonen <niko.salonen@almamedia.fi>
2019-07-04 13:43:00 +0300

    update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Lottokone',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    description: 'Generoi itsellesi lottonumerot. Varma voitto!',
+    author: '@nikosalonen',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
