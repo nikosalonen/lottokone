3e0f5709c84c8780341b5838b4eaef731eee419e
Niko Salonen <niko.salonen@almamedia.fi>
2019-07-05 10:38:11 +0300

    hooks!

---
+++ src/components/balls.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import chunk from 'lodash/chunk';
+import random from 'lodash/random';
+import findLastIndex from 'lodash/findLastIndex';
+import map from 'lodash/map';
+import shuffle from 'lodash/shuffle';
+import includes from 'lodash/includes';
+
+const getBalls = () => {
+  let numbers = chunk(shuffle(map(Array(40), (e, i) => i + 1)), 7);
+
+  const lastRow = findLastIndex(numbers);
+  while (numbers[lastRow].length < 7) {
+    const num = random(1, 40);
+    if (!includes(numbers[lastRow], num)) {
+      numbers[lastRow].push(num);
+    }
+  }
+  numbers = map(numbers, row => row.sort((a, b) => a - b));
+  return map(numbers, (row, i) => `<span class="row">Rivi ${i + 1}:  ${map(row, num => `<span class="number">${num}</span>`).join('')}</span>`).join('');
+};
+const Balls = () => {
+  const [balls, setBalls] = useState(getBalls());
+  return (
+    <>
+
+      <button type="button" onClick={() => setBalls(getBalls())}>Arvo uudet numerot</button>
+
+      <div id="rowContainer" dangerouslySetInnerHTML={{ __html: balls }} />
+
+    </>
+  );
+};
+export default Balls;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-import chunk from 'lodash/chunk';
-import random from 'lodash/random';
-import findLastIndex from 'lodash/findLastIndex';
-import map from 'lodash/map';
-import shuffle from 'lodash/shuffle';
-import includes from 'lodash/includes';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Balls from '../components/balls';
 
 
-let numbers = chunk(shuffle(map(Array(40), (e, i) => i + 1)), 7);
-
-const lastRow = findLastIndex(numbers);
-while (numbers[lastRow].length < 7) {
-  const num = random(1, 40);
-  if (!includes(numbers[lastRow], num)) {
-    numbers[lastRow].push(num);
-  }
-}
-numbers = map(numbers, row => row.sort((a, b) => a - b));
-const balls = map(numbers, (row, i) => `<span class="row">Rivi ${i + 1}:  ${map(row, num => `<span class="number">${num}</span>`).join('')}</span>`).join('');
 const IndexPage = () => (
   <Layout>
     <SEO title="Lottokone" keywords={['lotto', 'voitto', 'lottokone']} />
@@ -35,14 +19,14 @@ const IndexPage = () => (
       loksahtavat oikeaan järjestykseen riville.
     </p>
     <p>
-Koska 40 ei ole jaollinen seitsemällä, lisätään viimeiseen riviin kolme satunnaista numeroa. Eikö
-      numerot miellytä? Lataa sivu uudelleen!
+Koska 40 ei ole jaollinen seitsemällä, lisätään viimeiseen riviin kolme satunnaista numeroa.
     </p>
+
     <p>
 Voittorivisi on:
 
     </p>
-    <div id="rowContainer" dangerouslySetInnerHTML={{ __html: balls }} />
+    <Balls />
   </Layout>
 );
 
