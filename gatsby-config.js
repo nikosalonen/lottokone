3b2e5c18efa5d7b157a6ff7e9d53f7c2430c3201
Niko Salonen <niko.salonen@almamedia.fi>
2019-07-05 11:05:59 +0300

    GA head

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
       options: {
         trackingId: 'UA-137172555-1',
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
